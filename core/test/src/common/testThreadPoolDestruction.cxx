b910bf18c41c9107355e9f64a34d03ec40c532dd
tsw <tsw@4525493e-7705-40b1-a816-d608a930855b>
2015-06-11 14:07:35 +0000

    SWATCH, refs #1230 : Add test executable to reproduce seg-faults from thread pool / object destruction
    git-svn-id: svn+ssh://svn.cern.ch/reps/cactus/trunk/cactuscore/swatch@36945 4525493e-7705-40b1-a816-d608a930855b

---
+++ core/test/src/common/testThreadPoolDestruction.cxx
@@ -0,0 +1,52 @@
+
+
+#include "swatch/core/ThreadPool.hpp"
+#include "swatch/core/XParameterSet.hpp"
+#include "swatch/core/test/DummyCommand.hpp"
+#include "swatch/core/test/DummyHandler.hpp"
+
+
+using namespace swatch::core::test;
+
+struct CommandTestSetup {
+  CommandTestSetup():
+  handler(),
+  print(),
+  error(),
+  nothing(),
+  params(){
+    handler.Register<DummyCommand>("dummy_print");//, new DummyCommand(&handler, xdata::Integer(-33)));
+    handler.Register<DummyCommand>("dummy_error");//, new DummyCommand(&handler, xdata::Integer(-33)));
+    handler.Register<DummyCommand>("dummy_nada");//, new DummyCommand(&handler, xdata::Integer(-33)));
+
+    print = handler.getCommand("dummy_print");
+    error = handler.getCommand("dummy_error");
+    nothing = handler.getCommand("dummy_nada");
+
+    ((DummyCommand*) print)->registerParameter("todo", xdata::String("print"));
+    ((DummyCommand*) error)->registerParameter("todo", xdata::String("error"));
+    ((DummyCommand*) nothing)->registerParameter("todo", xdata::String("nothing"));
+  }
+  ~CommandTestSetup(){
+  }
+
+  swatch::core::test::DummyHandler handler;
+  swatch::core::Command* print, *error, *nothing;
+  swatch::core::XParameterSet params;
+};
+
+
+int main( int argc, const char* argv[] )
+{
+  CommandTestSetup setup;
+
+  swatch::core::XParameterSet params;
+
+  setup.print->exec( setup.params );
+
+  std::cout << "print command being executed " << std::endl;
+
+  //swatch::core::ThreadPool::reset();
+
+  return 0;
+}
