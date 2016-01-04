ed5fb98b9f5fc9a8b45f57d2b33b5178a33bd142
tsw <tsw@4525493e-7705-40b1-a816-d608a930855b>
2016-01-04 11:20:58 +0000

    SWATCH, refs #1499 : Add script that finds variables, functions and classes that don't conform to naming rules
    git-svn-id: svn+ssh://svn.cern.ch/reps/cactus/trunk/cactuscore/swatch@41326 4525493e-7705-40b1-a816-d608a930855b

--- dev_tools/createCppcheckDumps.sh
+++ dev_tools/createCppcheckDumps.sh
@@ -3,15 +3,13 @@
 SWATCH_DEV_TOOLS=$( readlink -f $(dirname $BASH_SOURCE)/ )
 SWATCH_ROOT=${SWATCH_DEV_TOOLS}/..
 
-INCLUDE_PATHS=-I/opt/cactus/include \
-   -I/opt/xdaq/include \
-   -I/opt/xdaq/include/linux \
-   -I${SWATCH_ROOT}/logger/include \
-   -I${SWATCH_ROOT}/core/include \
-   -I${SWATCH_ROOT}/processor/include \
-   -I${SWATCH_ROOT}/system/include \
+INCLUDE_PATHS="-I/opt/cactus/include -I/opt/xdaq/include -I/opt/xdaq/include/linux"
+INCLUDE_PATHS="${INCLUDE_PATHS} -I${SWATCH_ROOT}/logger/include"
+INCLUDE_PATHS="${INCLUDE_PATHS} -I${SWATCH_ROOT}/core/include"
+INCLUDE_PATHS="${INCLUDE_PATHS} -I${SWATCH_ROOT}/processor/include"
+INCLUDE_PATHS="${INCLUDE_PATHS} -I${SWATCH_ROOT}/system/include"
 
-
+echo INCLUDE_PATHS=${INCLUDE_PATHS}
 cppcheck ${INCLUDE_PATHS} --dump {logger,core,processor,system}/src/common/*.cpp
 
 
