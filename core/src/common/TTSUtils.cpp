0277a82f8ff8a86c2cc87a9124bfe4586b188105
thea <thea@4525493e-7705-40b1-a816-d608a930855b>
2016-02-17 13:31:47 +0000

    Added TTS state hex value to state string. Refs #1623
    git-svn-id: svn+ssh://svn.cern.ch/reps/cactus/trunk/cactuscore/swatch@42526 4525493e-7705-40b1-a816-d608a930855b

--- core/src/common/TTSUtils.cpp
+++ core/src/common/TTSUtils.cpp
@@ -4,12 +4,12 @@ namespace swatch {
 namespace core {
 namespace tts {
 
-const std::string kWarningStr="Warning Overflow";
-const std::string kOutOfSyncStr="Out of Sync";
-const std::string kBusyStr="Busy";
-const std::string kReadyStr="Ready";
-const std::string kErrorStr="Error";
-const std::string kDisconnectedStr="Disconnected";
+const std::string kWarningStr="Warning Overflow (0x1)";
+const std::string kOutOfSyncStr="Out of Sync (0x2)";
+const std::string kBusyStr="Busy (0x4)";
+const std::string kReadyStr="Ready (0x8)";
+const std::string kErrorStr="Error (0xc)";
+const std::string kDisconnectedStr="Disconnected (0xf)";
 const std::string kUnknownStr="Unknown";
 
 const std::string& codeToString( uint32_t aCode ) {
